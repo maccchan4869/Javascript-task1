29dd70a77c782675d17eacc278eec83b3f5ef037
Sho kuroiwa <kuroiwa.syo@gmail.com>
2021-10-11 21:38:36 +0900

    ここでコミットするのかな？ コミット⇒プッシュというやりかた？

--- task1.js
+++ task1.js
@@ -1,9 +1,19 @@
 const maxNum = 100;
 
-
-
-
-
+//2 は素数なので true を返す
+if(num === 2) {
+  console.log(num)
+} else {
+  for(i = 2; i < num; i++) {
+    //2以上の数で割ったとき余りが0になれば false を返す。つまり素数ではない。
+    if(num % i === 0) {
+      break
+    }
+    if(i + 1 === num) {
+      console.log(num)
+    }
+  }
+}
 
 for (var i = 1; i <= maxNum; i++) {
   console.log(i);
